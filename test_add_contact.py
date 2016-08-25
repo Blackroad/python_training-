c987573f2f5f76ee3aa31a2a7f83362a058db54b
Vladyslav Velychko <VladVell@gmail.com>
2016-08-25 21:36:15 +0300

    Revert "Добавлен класс Contacts и передача его объектов"
    This reverts commit 1a0605872d0101dd79645e9ae7f0e04b51bf12d7.
    
    # Conflicts:
    #	test_add_contact.py

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from contacts import Contacts
 
 import unittest
 
@@ -18,7 +17,7 @@ class test_add_contact(unittest.TestCase):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
-    def test_add_contact(self):
+    def test_test_add_contact(self):
         wd = self.wd
         # Login
         self.login(wd, username="admin", password="secret")
@@ -32,32 +31,35 @@ class test_add_contact(unittest.TestCase):
         self.login(wd, username="admin", password="secret")
         self.add_new_contact(wd, Contacts(first_name="", last_name="", initials="", nickname="",
                                           home_phone="", email=""))
+=======
+        self.add_new_contact(wd, "John", "Smith", "JS", "Johny", "38491123", "js_smith@yandex.ru")
+>>>>>>> parent of 1a06058... Добавлен класс Contacts и передача его объектов
         self.logout(wd)
 
     def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
 
-    def add_new_contact(self, wd, Contacts):
+    def add_new_contact(self, wd, first_name, last_name, initials, nickname, home_phone, email):
         # Init new contact
         wd.find_element_by_link_text("add new").click()
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(Contacts.firstname)
+        wd.find_element_by_name("firstname").send_keys(first_name)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(Contacts.lastname)
+        wd.find_element_by_name("lastname").send_keys(last_name)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(Contacts.initials)
+        wd.find_element_by_name("middlename").send_keys(initials)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(Contacts.nickname)
+        wd.find_element_by_name("nickname").send_keys(nickname)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(Contacts.homephone)
+        wd.find_element_by_name("home").send_keys(home_phone)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(Contacts.email)
+        wd.find_element_by_name("email").send_keys(email)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
     def login(self, wd, username, password):

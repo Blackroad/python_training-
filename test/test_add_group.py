b7935074cf503ccf27f8e088391a71225f584181
Vladyslav Velychko <VladVell@gmail.com>
2016-08-26 18:58:53 +0300

    Выделен класс для управления сессиями

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,14 +13,14 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="new1", header="new2", footer="new3"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
 
 #stage1
